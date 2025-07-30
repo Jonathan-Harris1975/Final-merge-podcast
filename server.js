c6f113332a7184f0689e3ece812995eb2de561f7
Jonathan-Harris1975 <johnbristol26@gmail.com>
2025-07-30 16:01:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const os = require('os');
 const crypto = require('crypto');
 const compression = require('compression');
 const { default: PQueue } = require('p-queue');
+const processQueue = new PQueue.default({ 
+  concurrency: os.cpus().length > 2 ? 2 : 1 
+});
 
 
 // Initialize environment and error handlers first
